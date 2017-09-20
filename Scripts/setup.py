29bddf67813e60dfa9f2b1d807a9a9e11008ba3b
Madhu Cheemla <mcheemala@kogentix.com>
2017-09-20 13:05:46 +0530

    Initial Commit

---
+++ Scripts/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ml_ecomh9',
+    version='1.8.0-SNAPSHOT',
+    py_modules=['ecom_h9'],
+    author='Kogentix, Inc.',
+    packages=find_packages(exclude=['ecom_h9.tests']),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'nose',
+        'nltk'
+        ],
+    entry_points={
+        'console_scripts': [
+            'ml_h9predict=ecom_h9.predict:predict_h9',
+        ]
+    },
+    include_package_data=True,
+    # test_suite='nose.collector',
+    # tests_require=['nose'],
+)
